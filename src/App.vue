4d71eb35dda96604d97cd194c99b028cb9e2459a
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-20 08:46:22 +0900

    버튼 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,74 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, onMounted } from 'vue';
+
+const monthCircular = ref(0);
+const monthLabel = ref("");
+
+const todayCircular = ref(0);
+const todayUseLabel = ref("");
+const todayTotalLabel = ref("");
+
+const timeNow = ref("");
 
 // 한 달 총 데이터 용량 (GB)
 const totalData = 11;
 
-// 현재 날짜와 시간
-const now = new Date();
 
-// 현재 월의 총 일 수 계산
-const daysInMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
 
-// 하루에 사용할 수 있는 데이터 계산
-const dailyData = totalData / daysInMonth;
 
-// 현재까지 경과한 시간 계산 (일 단위)
-const elapsedDays = now.getDate() - 1; // 오늘은 포함하지 않음
-const elapsedHours = now.getHours() / 24; // 시간 비율
-const elapsedTime = elapsedDays + elapsedHours;
+function getNow() {
+  const now = new Date();
+  timeNow.value = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+
+  // 현재 월의 총 일 수 계산
+  const daysInMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+
+  // 하루에 사용할 수 있는 데이터 계산
+  const dailyData = totalData / daysInMonth;
+
+  // 현재까지 경과한 시간 계산 (일 단위)
+  const elapsedDays = now.getDate() - 1; // 오늘은 포함하지 않음
+  const elapsedHours = now.getHours() / 24; // 시간 비율
+  const elapsedTime = elapsedDays + elapsedHours;
 
 // 현재까지 사용할 수 있는 데이터 계산
-const availableData = computed(() => (elapsedTime / daysInMonth) * totalData);
+  const availableData = (elapsedTime / daysInMonth) * totalData;
 
 // 오늘 현재 시간까지 사용할 수 있는 데이터 계산
-const todayAvailableData = computed(() => {
-  const todayElapsedHours = now.getHours() / 24; // 오늘 경과한 시간 비율
-  return todayElapsedHours * dailyData;
+  const todayAvailableData =  (now.getHours() / 24) * dailyData; // 오늘 경과한 시간 비율
+
+
+  monthCircular.value = ((totalData - availableData) / totalData) * 100;
+  monthLabel.value =  (totalData - availableData).toFixed(1);
+  todayCircular.value = ((dailyData - todayAvailableData) / dailyData) * 100;
+  todayUseLabel.value = (todayAvailableData * 1024).toFixed(0);
+  todayTotalLabel.value = (dailyData * 1024).toFixed(0);
+ 
+};
+
+onMounted(() => {
+  getNow();
 });
 
-function closePage() {
-  window.close(); // 브라우저 창 닫기
-};
+
 
 </script>
 
 <template>
   <div class="center-container">
     <div class="current-time">
-      <v-chip color="primary">{{ new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }) }}</v-chip>
+      <v-chip color="primary" @click="getNow()">{{ timeNow }}</v-chip>
     </div>
 
-    <v-progress-circular :model-value="((totalData - availableData) / totalData) * 100" :rotate="360" :size="200"
+    <v-progress-circular :model-value="monthCircular" :rotate="360" :size="200"
       :width="20" color="primary">
-      <span class="monthUse">{{ totalData - availableData.toFixed(1) }} GB</span></v-progress-circular><p></p>
+      <span class="monthUse">{{ monthLabel }} GB</span></v-progress-circular><p></p>
 
-    <v-progress-circular :model-value="((dailyData - todayAvailableData) / dailyData) * 100" :rotate="360" :size="120"
+    <v-progress-circular :model-value="todayCircular" :rotate="360" :size="120"
       :width="14" color="teal">
       <div>
-        <span class="todayUse">{{ (todayAvailableData * 1024).toFixed(0) }} MB</span>
-        <span class="todayTotal">/ {{ (dailyData * 1024).toFixed(0) }} MB</span>
+        <span class="todayUse">{{ todayUseLabel }} MB</span>
+        <span class="todayTotal">/ {{ todayTotalLabel }} MB</span>
       </div>
     </v-progress-circular>
   </div>
