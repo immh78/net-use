c4bb6e3e21a7768bbe08ab1f5456e0c98b0a675c
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-20 00:08:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,17 +18,17 @@ export default defineConfig({
       start_url: '/net-use/',
       icons: [
         {
-          src: 'ratio-192-icon.png',
+          src: 'ratio-192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: 'ratio-icon.png',
+          src: 'ratio.png',
           sizes: '512x512',
           type: 'image/png'
         },
         {
-          src: 'ratio-icon.png',
+          src: 'ratio.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable'
